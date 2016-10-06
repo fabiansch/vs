0d3b34094cf3516a6cb0ccdb0999889645f33f9b
Fabian <stahlschiff@web.de>
2016-10-06 08:57:45 +0200

    Update vs15.cpp

--- vs15.cpp
+++ vs15.cpp
@@ -10,7 +10,7 @@ using namespace caf;
 behavior adder(event_based_actor* self) {
   return {
     [](int x, int y) {
-      return x + y;
+      return x + y; // Ist implizit die Antwortnachricht
     }
   };
 }
@@ -19,18 +19,26 @@ void caf_main(actor_system& sys) {
   actor a = sys.spawn(adder);
   // scoped actor for ad hoc communication
   scoped_actor self{sys};
-  self->request(a, infinite, 1, 2).receive(
-    [&](int z) {
+  self->request(a, infinite, 1, 2).receive(   // infinite timeout
+    [&](int z) {                              // eckige Klammern --> Lamda/Anonyme Funktion, & --> Referenzaufruf
       cout << "1 + 2 = " << z << "\n";
     },
     [&](error& err) {
       cout << "Error: " << sys.render(err) << "\n";
     }
   );
-  // wrapping a into a function object
+  // wrapping a into a function object  // Das gleiche wie oben, nur mit view_function_view
   auto f = make_function_view(a);
+  // auto z = f(1, 2);
+  // if(z) {
+  //    cout<< f(1,2) defined
+  // } else {
+  //   cout<< sys.render(z.error()) << "\n";
+  // }
   cout << "f(1, 2) = " << to_string(f(1, 2)) << "\n";
 }
 
 CAF_MAIN(io::middleman)
 
+  
+//  ./build/vs15
